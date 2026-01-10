5bda0dbd3a6edadd15f6e8af7f6c78b8065115f4
Zero <nhthang312@gmail.com>
2026-01-10 21:03:04 +0700

    feat(auth): Implement user model and use cases for authentication flow

--- lexilingo_app/lib/features/auth/presentation/providers/auth_provider.dart
+++ lexilingo_app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lexilingo_app/core/usecase/usecase.dart';
 import 'package:lexilingo_app/features/auth/domain/entities/user_entity.dart';
-import 'package:lexilingo_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:lexilingo_app/features/auth/domain/usecases/get_current_user_usecase.dart';
+import 'package:lexilingo_app/features/auth/domain/usecases/sign_in_with_google_usecase.dart';
+import 'package:lexilingo_app/features/auth/domain/usecases/sign_out_usecase.dart';
 
 class AuthProvider extends ChangeNotifier {
-  final AuthRepository repository;
+  final SignInWithGoogleUseCase signInWithGoogleUseCase;
+  final SignOutUseCase signOutUseCase;
+  final GetCurrentUserUseCase getCurrentUserUseCase;
   UserEntity? _user;
 
-  AuthProvider({required this.repository}) {
+  AuthProvider({
+    required this.signInWithGoogleUseCase,
+    required this.signOutUseCase,
+    required this.getCurrentUserUseCase,
+  }) {
     _checkCurrentUser();
   }
 
@@ -14,13 +23,13 @@ class AuthProvider extends ChangeNotifier {
   bool get isAuthenticated => _user != null;
 
   Future<void> _checkCurrentUser() async {
-    _user = await repository.getCurrentUser();
+    _user = await getCurrentUserUseCase(NoParams());
     notifyListeners();
   }
 
   Future<void> signIn() async {
     try {
-      _user = await repository.signInWithGoogle();
+      _user = await signInWithGoogleUseCase(NoParams());
       notifyListeners();
     } catch (e) {
       debugPrint("Sign in error: $e");
@@ -28,7 +37,7 @@ class AuthProvider extends ChangeNotifier {
   }
 
   Future<void> signOut() async {
-    await repository.signOut();
+    await signOutUseCase(NoParams());
     _user = null;
     notifyListeners();
   }
