c115e6d5c548986f5c996f50ec0e23dde3704e05
Zero <nhthang312@gmail.com>
2026-01-24 14:01:47 +0700

    refactor: remove deprecated project structure (LexiLingo_backend, UI, lexilingo_app)

--- lexilingo_app/lib/features/chat/presentation/widgets/widgets.dart
+++
@@ -1,6 +0,0 @@
-/// Chat presentation widgets
-/// 
-/// This file exports all widgets used in the chat feature
-export 'message_bubble.dart';
-export 'session_list_drawer.dart';
-export 'markdown_message_content.dart';
--- lexilingo_app/lib/features/vocabulary/domain/repositories/vocab_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:lexilingo_app/features/vocabulary/domain/entities/vocab_word.dart';
-
-abstract class VocabRepository {
-  Future<List<VocabWord>> getWords();
-  Future<void> addWord(VocabWord word);
-}
