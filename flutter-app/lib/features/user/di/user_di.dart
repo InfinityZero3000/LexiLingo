22ed3d647ea3b394e85c41ae6eea73f4efbdb9f4
Zero <nhthang312@gmail.com>
2026-02-01 21:20:24 +0700

    feat(user): add user stats and weekly activity
    - Add user stats entity and model
    - Add weekly activity entity and model
    - Add user backend data source
    - Add get user stats usecase
    - Add get weekly activity usecase
    - Update user repository and DI
    - Support user statistics tracking

--- flutter-app/lib/features/user/di/user_di.dart
+++ flutter-app/lib/features/user/di/user_di.dart
@@ -1,4 +1,5 @@
 import 'package:lexilingo_app/core/di/service_locator.dart';
+import 'package:lexilingo_app/core/network/api_client.dart';
 import 'package:lexilingo_app/core/services/database_helper.dart';
 import 'package:lexilingo_app/core/services/firestore_service.dart';
 import 'package:lexilingo_app/core/services/progress_firestore_data_source.dart';
@@ -9,6 +10,7 @@ import 'package:lexilingo_app/features/user/data/datasources/settings_local_data
 import 'package:lexilingo_app/features/user/data/datasources/settings_local_data_source_web.dart';
 import 'package:lexilingo_app/features/user/data/datasources/streak_local_data_source.dart';
 import 'package:lexilingo_app/features/user/data/datasources/streak_local_data_source_web.dart';
+import 'package:lexilingo_app/features/user/data/datasources/user_backend_data_source.dart';
 import 'package:lexilingo_app/features/user/data/datasources/user_firestore_data_source.dart';
 import 'package:lexilingo_app/features/user/data/datasources/user_local_data_source.dart';
 import 'package:lexilingo_app/features/user/data/datasources/user_local_data_source_web.dart';
@@ -25,6 +27,8 @@ import 'package:lexilingo_app/features/user/domain/usecases/get_current_streak_u
 import 'package:lexilingo_app/features/user/domain/usecases/get_goal_history_usecase.dart';
 import 'package:lexilingo_app/features/user/domain/usecases/get_settings_usecase.dart';
 import 'package:lexilingo_app/features/user/domain/usecases/get_today_goal_usecase.dart';
+import 'package:lexilingo_app/features/user/domain/usecases/get_user_stats_usecase.dart';
+import 'package:lexilingo_app/features/user/domain/usecases/get_weekly_activity_usecase.dart';
 import 'package:lexilingo_app/features/user/domain/usecases/set_daily_goal_usecase.dart';
 import 'package:lexilingo_app/features/user/domain/usecases/get_user_usecase.dart';
 import 'package:lexilingo_app/features/user/domain/usecases/update_daily_progress_usecase.dart';
@@ -73,10 +77,16 @@ void registerUserModule({required bool skipDatabase}) {
     );
   }
 
+  // Register Backend Data Source
+  sl.registerLazySingleton<UserBackendDataSource>(
+    () => UserBackendDataSourceImpl(apiClient: sl<ApiClient>()),
+  );
+
   sl.registerLazySingleton<UserRepository>(
     () => UserRepositoryImpl(
       localDataSource: sl(),
       firestoreDataSource: sl.isRegistered<UserFirestoreDataSource>() ? sl() : null,
+      backendDataSource: sl<UserBackendDataSource>(),
     ),
   );
   sl.registerLazySingleton<SettingsRepository>(
@@ -93,6 +103,8 @@ void registerUserModule({required bool skipDatabase}) {
   sl.registerLazySingleton(() => CreateUserUseCase(repository: sl()));
   sl.registerLazySingleton(() => UpdateUserUseCase(repository: sl()));
   sl.registerLazySingleton(() => UpdateUserStatsUseCase(repository: sl()));
+  sl.registerLazySingleton(() => GetUserStatsUseCase(sl<UserRepository>()));
+  sl.registerLazySingleton(() => GetWeeklyActivityUseCase(sl<UserRepository>()));
   sl.registerLazySingleton(() => GetSettingsUseCase(repository: sl()));
   sl.registerLazySingleton(() => UpdateSettingsUseCase(repository: sl()));
   sl.registerLazySingleton(() => GetTodayGoalUseCase(repository: sl()));
